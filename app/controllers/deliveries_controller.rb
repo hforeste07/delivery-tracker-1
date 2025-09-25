2af953c5ccad059d89e2ef2ce0adf3d33b03209c
Heather Forester <hforeste07@gmail.com>
2025-09-25 19:24:52 +0000

    got the button to work

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -39,16 +39,13 @@ class DeliveriesController < ApplicationController
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
     the_delivery.user_id = current_user.id
-    the_delivery.description = params.fetch("query_description")
-    the_delivery.details = params.fetch("query_details")
-    the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.arrived = "1"
+    the_delivery.arrived = true
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries/#{the_delivery.id}", { :notice => "Delivery updated successfully."} )
+      redirect_to("/deliveries", { :notice => "Delivery updated successfully."} )
     else
-      redirect_to("/deliveries/#{the_delivery.id}", { :alert => the_delivery.errors.full_messages.to_sentence })
+      redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
   end
 
