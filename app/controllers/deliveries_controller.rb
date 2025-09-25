62eabb9136dacee4f0d85a8ae02f505438bbf475
Heather Forester <hforeste07@gmail.com>
2025-09-25 18:59:18 +0000

    still working on it

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -4,7 +4,7 @@ class DeliveriesController < ApplicationController
   def index
     matching_deliveries = Delivery.all
     @list_of_deliveries = matching_deliveries.order({ :created_at => :desc })
-    
+
     render({ :template => "deliveries/index" })
   end
 
@@ -20,7 +20,7 @@ class DeliveriesController < ApplicationController
 
   def create
     the_delivery = Delivery.new
-    the_delivery.user_id = params.fetch("query_user_id")
+    the_delivery.user_id = current_user.id
     the_delivery.description = params.fetch("query_description")
     the_delivery.details = params.fetch("query_details")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
@@ -28,7 +28,7 @@ class DeliveriesController < ApplicationController
 
     if the_delivery.valid?
       the_delivery.save
-      redirect_to("/deliveries", { :notice => "Delivery created successfully." })
+      redirect_to("/deliveries", { :notice => "Added to list" })
     else
       redirect_to("/deliveries", { :alert => the_delivery.errors.full_messages.to_sentence })
     end
@@ -38,11 +38,11 @@ class DeliveriesController < ApplicationController
     the_id = params.fetch("path_id")
     the_delivery = Delivery.where({ :id => the_id }).at(0)
 
-    the_delivery.user_id = params.fetch("query_user_id")
+    the_delivery.user_id = current_user.id
     the_delivery.description = params.fetch("query_description")
     the_delivery.details = params.fetch("query_details")
     the_delivery.supposed_to_arrive_on = params.fetch("query_supposed_to_arrive_on")
-    the_delivery.arrived = params.fetch("query_arrived", false)
+    the_delivery.arrived = "1"
 
     if the_delivery.valid?
       the_delivery.save
